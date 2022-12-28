639a6831c6e1bdfa15d9a71afe0cf71a9e30be28
slzhang-git <92975752+slzhang-git@users.noreply.github.com>
2022-12-28 14:25:54 +0100

    Update train.py
    avoid error in executing of evaluate.py

--- utils/train.py
+++ utils/train.py
@@ -7,14 +7,17 @@ from tensorflow.keras.utils import plot_model
 from tensorflow.keras.losses import binary_crossentropy
 from sklearn.model_selection import cross_val_score
 from model import SolNet
+import os   # added
+import matplotlib.pyplot as plt     # added
 
 def train():
   batch_size = 32
-  location = 'dataset/'
+  location = os.path.realpath(os.getcwd() + '/../dataset/')
   label_mode = 'binary'
   seed = 10 #changed for each fold made manually
   epochs=30
   class_names = ['clean', 'dirty']
+  in_size = [227, 227, 3]   #   added
 
   tr_dataset = image_dataset_from_directory(directory=location, label_mode= label_mode, class_names=class_names,
                                             seed=seed, labels='inferred', image_size=in_size[:-1], 
@@ -24,10 +27,16 @@ def train():
                                             seed=seed, labels='inferred', image_size=in_size[:-1],
                                             subset = 'validation', batch_size=batch_size, validation_split=.2)
 
-  in_size = [227, 227, 3]
+  #in_size = [227, 227, 3]
   SolNet = SolNet(in_size)
   history = SolNet.fit(tr_dataset, validation_data=val_dataset, epochs=epochs, batch_size=batch_size)
   return history
 
 if __name__ == "__main__":
-  train()
+  history = train()   # revised #
+  plt.plot(history.history['loss'])     # added
+  plt.plot(history.history['acc'])     # added
+  plt.title('acc loss vs epoch')     # added
+  plt.xlabel('epoch')     # added
+  plt.legend(['loss', 'acc'], loc='upper left')     # added
+  plt.show()     # added
