7b016b58c60965134a316693cdcba44f79221717
slzhang-git <92975752+slzhang-git@users.noreply.github.com>
2023-02-28 10:34:45 +0100

    Update train.py

--- utils/train.py
+++ utils/train.py
@@ -7,36 +7,44 @@ from tensorflow.keras.utils import plot_model
 from tensorflow.keras.losses import binary_crossentropy
 from sklearn.model_selection import cross_val_score
 from model import SolNet
-import os   # added
-import matplotlib.pyplot as plt     # added
+import os  # added
+import matplotlib.pyplot as plt  # added
+from tensorflow.keras.callbacks import CSVLogger   # added
+import time
+
 
 def train():
-  batch_size = 32
-  location = os.path.realpath(os.getcwd() + '/../dataset/')
-  label_mode = 'binary'
-  seed = 10 #changed for each fold made manually
-  epochs=30
-  class_names = ['clean', 'dirty']
-  in_size = [227, 227, 3]   #   added
+    batch_size = 32
+    location = os.path.realpath(os.getcwd() + '/../dataset/')  # location = 'dataset/'
+    label_mode = 'binary'
+    seed = 10  # changed for each fold made manually
+    epochs = 600  # 30
+    class_names = ['clean', 'dirty']
+    in_size = [227, 227, 3]  # added
 
-  tr_dataset = image_dataset_from_directory(directory=location, label_mode= label_mode, class_names=class_names,
-                                            seed=seed, labels='inferred', image_size=in_size[:-1], 
-                                            subset = 'training', batch_size=batch_size, validation_split=.2)
+    tr_dataset = image_dataset_from_directory(directory=location, label_mode=label_mode, class_names=class_names,
+                                              seed=seed, labels='inferred', image_size=in_size[:-1],
+                                              subset='training', batch_size=batch_size, validation_split=.2)
 
-  val_dataset = image_dataset_from_directory(directory=location, label_mode= label_mode, class_names=class_names,
-                                            seed=seed, labels='inferred', image_size=in_size[:-1],
-                                            subset = 'validation', batch_size=batch_size, validation_split=.2)
+    val_dataset = image_dataset_from_directory(directory=location, label_mode=label_mode, class_names=class_names,
+                                               seed=seed, labels='inferred', image_size=in_size[:-1],
+                                               subset='validation', batch_size=batch_size, validation_split=.2)
 
-  #in_size = [227, 227, 3]
-  SolNet = SolNet(in_size)
-  history = SolNet.fit(tr_dataset, validation_data=val_dataset, epochs=epochs, batch_size=batch_size)
-  return history
+    #in_size = [227, 227, 3]
+    SolNet1 = SolNet(in_size)
+    csv_logger = CSVLogger(str(epochs) + '-epoch-training.log', separator=',', append=False) # added
+    #history = SolNet1.fit(tr_dataset, validation_data=val_dataset, epochs=epochs, batch_size=batch_size)
+    history = SolNet1.fit(tr_dataset, validation_data=val_dataset, epochs=epochs, batch_size=batch_size, callbacks=[csv_logger])
+    return history
+
 
 if __name__ == "__main__":
-  history = train()   # revised #
-  plt.plot(history.history['loss'])     # added
-  plt.plot(history.history['acc'])     # added
-  plt.title('acc loss vs epoch')     # added
-  plt.xlabel('epoch')     # added
-  plt.legend(['loss', 'acc'], loc='upper left')     # added
-  plt.show()     # added
+    start_time = time.time()        # added
+    history = train()  # revised #   train()
+    run_time = time.time() - start_time
+    plt.plot(history.history['acc'])  # added
+    plt.plot(history.history['val_acc'])  # added
+    plt.title('accuracy vs epoch. Running time: ' + str(run_time) + ' seconds')  # added
+    plt.xlabel('epoch')  # added
+    plt.legend(['train accuracy', 'validation accuracy'], loc='upper left')  # added
+    plt.show()  # added
